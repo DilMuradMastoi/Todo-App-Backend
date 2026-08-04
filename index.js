5baba99f252033e91bfdc5b810da1e442c052629
DilMuradMastoi <webdeveloper1234567890003@gmail.com>
2026-08-05 01:31:14 +0500

    changes add

--- index.js
+++ index.js
@@ -8,7 +8,10 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "https://new-todo-app-swart.vercel.app",
+    origin: [
+      "http://localhost:5173",
+      "https://new-todo-app-brown.vercel.app",
+    ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
